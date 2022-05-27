a68341da184c0f9d7a424912dd13c8c76e73a3da
Rana <mra309@compute-gpu-2-3.local>
2022-05-27 01:30:38 -0400

    added comments

--- src/element_interactive_density.py
+++ src/element_interactive_density.py
@@ -1,72 +1,77 @@
 #!/usr/bin/env python
+
+"""
+Introduction:
+    EID: element interactive density
+
+Author:
+    Masud Rana (masud.rana@uky.edu)
+    
+Date last modified:
+    May 26, 2022
+
+"""
+
 import numpy as np
 
 import numba_utils_methods as nmb
 
 
-
 class ElementInteractiveDensity:
-	def __init__(self, kernel_type='exponential', kernel_tau=1.0,
-	             kernel_power=2.0, ligand_vdW=1.7, protein_vdW=1.5):
+    def __init__(self, kernel_type='exponential', kernel_tau=1.0,
+                 kernel_power=2.0, ligand_vdW=1.7, protein_vdW=1.5):
 
-		self.kernel_type = kernel_type
-		self.kernel_tau = kernel_tau
-		self.kernel_power = kernel_power
-		
-		self.ligand_vdW = ligand_vdW
-		self.protein_vdW = protein_vdW
+        self.kernel_type = kernel_type
+        self.kernel_tau = kernel_tau
+        self.kernel_power = kernel_power
 
-		self.kernel_function = self.build_kernel_function(kernel_type)
+        self.ligand_vdW = ligand_vdW
+        self.protein_vdW = protein_vdW
 
-	def build_kernel_function(self, kernel_type):
-		if kernel_type[0] in ['E', 'e']:
-			return self.exponential_function
-		elif kernel_type[0] in ['L', 'l']:
-			return self.lorentz_function
+        self.kernel_function = self.build_kernel_function(kernel_type)
 
+    def build_kernel_function(self, kernel_type):
+        if kernel_type[0] in ['E', 'e']:
+            return self.exponential_function
+        elif kernel_type[0] in ['L', 'l']:
+            return self.lorentz_function
 
-	def exponential_function(self, atomic_distance):
+    def exponential_function(self, atomic_distance):
 
-		eta = self.kernel_tau * (self.ligand_vdW + self.protein_vdW)
+        eta = self.kernel_tau * (self.ligand_vdW + self.protein_vdW)
 
-		phi = np.exp(-(atomic_distance/eta) ** self.kernel_power)
-    
-		return np.round(phi, 5)
+        phi = np.exp(-(atomic_distance/eta) ** self.kernel_power)
 
-	def lorentz_function(self, atomic_distance):
+        return np.round(phi, 5)
 
-		eta = self.kernel_tau * (self.ligand_vdW + self.protein_vdW)
+    def lorentz_function(self, atomic_distance):
 
-		phi = 1 / (1 + atomic_distance/eta) ** self.kernel_power
-    
-		return np.round(phi, 5)
+        eta = self.kernel_tau * (self.ligand_vdW + self.protein_vdW)
 
+        phi = 1 / (1 + atomic_distance/eta) ** self.kernel_power
 
-	def atomic_density(self, grid_point_x, grid_point_y, grid_point_z,
-	                   interactive_atoms_coordinates):
+        return np.round(phi, 5)
 
-		cloudpoint = np.array([[grid_point_x, grid_point_y, grid_point_z]])
+    def atomic_density(self, grid_point_x, grid_point_y, grid_point_z,
+                       interactive_atoms_coordinates):
 
-		d = nmb.distance_matrix(cloudpoint, interactive_atoms_coordinates)
-	    
-		phi = self.kernel_function(d)
+        cloudpoint = np.array([[grid_point_x, grid_point_y, grid_point_z]])
 
-		return phi.sum(axis=1)
+        d = nmb.distance_matrix(cloudpoint, interactive_atoms_coordinates)
 
+        phi = self.kernel_function(d)
 
-	def main(self, nx, ny, nz, x, y, z, ei_xyz):
+        return phi.sum(axis=1)
 
-	    rtn = np.zeros([nx, ny, nz])
+    def main(self, nx, ny, nz, x, y, z, ei_xyz):
 
-	    for i in range(nx):
-	        for j in range(ny):
-	            for k in range(nz):
-	                rtn[i, j, k] = self.atomic_density(
-	                    x[i], y[j], z[k], ei_xyz)
+        rtn = np.zeros([nx, ny, nz])
 
-	    return rtn
+        for i in range(nx):
+            for j in range(ny):
+                for k in range(nz):
+                    rtn[i, j, k] = self.atomic_density(
+                        x[i], y[j], z[k], ei_xyz)
 
+        return rtn
 
-if __name__=="__main__":
-
-	eid_class = ElementInteractiveDensity()
